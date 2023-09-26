0b737b824d5a99ff0f851c0c92735c394c712d2a
Dido <100509456+Dido2002@users.noreply.github.com>
2023-09-26 20:22:20 +0300

    Add files via upload

---
+++ Quotes.js
@@ -0,0 +1,35 @@
+// quotes.js
+const quotesData = [
+    {
+      text: "The present changes the past. Looking back you do not find what you left behind",
+      author: "Kiran Desai",
+    },
+    {
+      text: "That's the thing about books. They let you travel without moving your feet",
+      author: "Jhumpa Lahiri",
+    },
+    {
+      text: "Let us remember: One book, one pen, one child, and one teacher can change the world",
+      author: "Malala Yousafzai",
+    },
+    {
+      text: "You can never get a cup of tea large enough or a book long enough to suit me",
+      author: "C.S. Lewis",
+    },
+    {
+      text: "So many books, so little time",
+      author: "Frank Zappa",
+    },
+    {
+      text: "Good friends, good books, and a sleepy conscience: this is the ideal life.",
+      author: "Mark Twain",
+    },
+    {
+      text: "Fairy tales are more than true: not because they tell us that dragons exist, but because they tell us that dragons can be beaten",
+      author: "Neil Gaiman, Coraline",
+    }
+ 
+    // Add more quotes and authors as needed
+  ];
+  
+  export default quotesData;
