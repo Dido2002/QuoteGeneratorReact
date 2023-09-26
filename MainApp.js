73a7390e49cb52c0d6c090e0b70cea20f44ea37d
Dido <100509456+Dido2002@users.noreply.github.com>
2023-09-26 20:22:02 +0300

    Add files via upload

---
+++ MainApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import QuotesGenerator from './QuotesGenerator';
+
+export default function MainApp(){
+    return(
+        <div className="mainContainer">
+            <QuotesGenerator/>
+        </div>
+    )
+}
